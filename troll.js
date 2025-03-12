0dc569816284b9300be9ac2ba4d51eccb3a91515
poomv3 <ericdraganoiu@gmail.com>
2025-03-12 14:55:01 +0000

    works better now
    the click actually shows something

--- troll.js
+++ troll.js
@@ -25,3 +25,18 @@ function troll() {
         console.log("true");
     }
 }
+
+let trollClick = document.querySelector('.troll');
+
+function spongebob() {
+    let body = document.querySelector('div').parentElement;
+    console.log('clicked');
+    let img = document.createElement('img');
+    img.classList.add('image');
+    img.src = 'https://cdn.discordapp.com/attachments/1250821141359104161/1349394921165029417/5D7BE8C6-DA12-43C5-A090-18A4C29AA0F0_1_105_c.jpeg?ex=67d2f173&is=67d19ff3&hm=c1a4e986fe2a2ad5aed29d3847d0f13dcbfd69e4cb6510248d91df3b9ed380f9&';
+    body.append(img);
+
+}
+
+trollClick.addEventListener("click", spongebob);
+// trollHover.addEventListener("onmouseover", spongebob);
