8713e96cb3730e056d9228c91084735bebc65892
poomv3 <ericdraganoiu@gmail.com>
2025-03-12 15:02:18 +0000

    added text

--- troll.js
+++ troll.js
@@ -36,6 +36,11 @@ function spongebob() {
     img.src = 'https://cdn.discordapp.com/attachments/1250821141359104161/1349394921165029417/5D7BE8C6-DA12-43C5-A090-18A4C29AA0F0_1_105_c.jpeg?ex=67d2f173&is=67d19ff3&hm=c1a4e986fe2a2ad5aed29d3847d0f13dcbfd69e4cb6510248d91df3b9ed380f9&';
     body.append(img);
 
+    let text = document.createElement('p');
+    text.classList.add('win-text');
+    text.innerText = "you win.";
+    body.append(text);
+
 }
 
 trollClick.addEventListener("click", spongebob);
