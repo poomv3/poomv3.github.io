e143888b9ef1e7b77146a538523d5721798358cb
poomv3 <ericdraganoiu@gmail.com>
2025-12-30 14:45:12 +0000

    yes

--- index.js
+++ index.js
@@ -19,45 +19,30 @@ let dark = true;
 // }
 
 function lightMode() {
-    let tog = document.body;
+    let tog = document.querySelector("body");
     let para = document.querySelectorAll("p");
-
     tog.classList.toggle("light");
     for (let x = 0; x < para.length; x++) {
         para[x].classList.toggle("light");
     }
-
+    console.log(tog.className)
     if (tog.className == "light") {
         let sun = document.querySelector(".mode");
-        tog.id = "sun";
+        sun.id = "sun";
         sun.src = "sun (1).svg";
+        console.log(1);
     } else {
-        let moon = document.querySelector(".mode")
+        tog.classList.toggle("dark");
+        let moon = document.querySelector(".mode");
         moon.id = "moon";
-        sun.src = "moon (2).svg";
+        moon.src = "moon (2).svg";
+        console.log(2);
     }
 
     
 }
 
-// function darkMode() {
-//     let moon = document.querySelector(".mode")
-//     moon.id = "moon";
-//     sun.src = "moon (2).svg";
-//     document.querySelector("body").style.animation = "light-dark 1s";
-//     document.querySelector("body").style.backgroundColor = "rgb(17, 17, 17);";
-
-//     let para = document.querySelectorAll("p");
-//     for (let x = 0; x < para.length; x++) {
-//         para[x].style.background = "url(wood.jpg) no-repeat";
-//         para[x].style.backgroundClip = "text";
-//     }
-
-//     dark = true;
-// }
-
 let mode = document.querySelector(".mode");
-console.log(mode.id);
 mode.addEventListener("click", lightMode);
 
 
