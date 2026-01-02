11d4f472dff256c7de5df4ac8e5d2c982c430a89
poomv3 <ericdraganoiu@gmail.com>
2026-01-02 20:14:24 +0000

    Summary (required)

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-let dark = true;
-
-// function lightMode() {
-//     let sun = document.querySelector(".mode");
-//     sun.id = "sun";
-//     sun.src = "sun (1).svg";
-//     document.querySelector("body").style.animation = "dark-light 1s";
-//     document.querySelector("body").style.backgroundColor = "white";
-
-//     let para = document.querySelectorAll("p");
-//     for (let x = 0; x < para.length; x++) {
-//         para[x].style.background = "url(obsidian.jpg) no-repeat";
-//         para[x].style.backgroundClip = "text";
-
-//     }
-
-//     console.log(sun.id)
-//     dark = false;
-// }
-
 function lightMode() {
     let tog = document.querySelector("body");
     let para = document.querySelectorAll("p");
